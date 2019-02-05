dc0bb4f005f3b148a04dcfa3e72f279a47be7498
Jacob Garcia <jdcia@github.com>
2019-02-05 23:44:22 +0000

    Added some networking stuff

--- Networking/listen.cpp
+++ Networking/listen.cpp
@@ -5,17 +5,20 @@
 
 
 
-int main(argv[], argc){
+int main(int argc, char **argv){
 
     QUdpSocket socket = new QUdpSocket();
     socket->bind(QHostAddress::Localhost, 5000);
 
-    connect(socket, SIGNAL(readyRead()), this),
+    connect();
 
     while(true){
         if(socket->hasPendingDatagrams){
             QNetworkDatagram gram = socket->receiveDatagram();
             //do stuff with data gram maybe start threading here if needed.
+            QByteArray data = gram.data();
+            std::cout << data.toStdString << endl;
+
         }
 
     }

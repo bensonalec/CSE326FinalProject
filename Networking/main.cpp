2cdf4ce5bf944e97ad0106a1dba9b073ccca6b49
Jacob Garcia <jdcia@github.com>
2019-02-12 07:53:35 +0000

    Had an idea at 1am did not working im going back to sleep

--- Networking/main.cpp
+++ Networking/main.cpp
@@ -1,12 +1,18 @@
+#include <QCoreApplication>
+#include "listen.h"
 
 
-int main(){
+int main(int argc, char **argv){
     
+    //QCoreApplication a(argc, argv);
+    
+   // MyUDP client;
+    
+    //client.HelloUDP();
 
+    Listen client;
 
+    client.start_socket();
 
-
-
-
-    return a.exec();
+    return 0;
 }

11511f9a1a0dea784329094144ed26b50dc6fee6
Alec Benson <bensonalec@Jamess-MacBook-Pro-2.local>
2019-03-02 14:42:20 -0700

    pushing latest server for jacob

--- Test Client/MyServer2.java
+++ Test Client/MyServer2.java
@@ -6,6 +6,7 @@ import java.util.Map;
 public class MyServer2 {
     private static ServerSocket sock;
     private static Socket recSock;
+
     public static void main(String[] args) {
         try {
             sock = new ServerSocket(5000);
@@ -14,8 +15,9 @@ public class MyServer2 {
             Map<String,ConnectionSock> connectionsList = new HashMap <String,ConnectionSock>();
             //Map for the IP, socket
             Map<String,ConnectionSock> connectionsListIP = new HashMap <String,ConnectionSock>();
+                        
 
-            //This should end up (modified of course) being the accept Connections Socket
+            //This should end up (modified of course) being the accept Connections threads
             //Creates socket object for 2 connections, as its not threaded it is blocked until it receives a connection, then a bytestream, then a second connection
             for(i=0;i<2;i++) {
                 //Accepts the connection
@@ -30,7 +32,7 @@ public class MyServer2 {
                 connectionsList.put(newSock.name,newSock);
                 connectionsListIP.put(newSock.ip,newSock);
                 
-                System.out.println(newSock.name + ": " + newSock.ip);
+                System.out.println(newSock.type + "\t" + newSock.name + ": " + newSock.ip);
                 
             }
             //This should end up being the routing and sending thread
@@ -38,7 +40,7 @@ public class MyServer2 {
             for(String key: connectionsList.keySet()) {
                 ConnectionSock temp = connectionsList.get(key);
                 //Case for the android device, right now its mapped only to my personal phone
-                if(key.equals("bensonalec@ONEPLUS A5010")) {
+                if(temp.type.equals("phone")) {
                     temp.sendTo("This should go to phone");
                     temp.closeSock();
                 }
@@ -50,7 +52,6 @@ public class MyServer2 {
             }            
 
             System.out.println("Finished Routing");
-              
             
         } catch (IOException e) {
             System.out.println("IOException");
@@ -58,13 +59,89 @@ public class MyServer2 {
 
     }
 
-    private static class ConnectionSock {
+    public static class notification {
+        String title = null;
+        String user = null;
+        String device = null;
+        Socket socket = null;
+        String content = null;
+        String app = null;
+        String raw = null;
+
+        private String getTitle() {
+            return title;
+        }
+        private void setTitle(String title) {
+            this.title = title;
+        }
+        private String getUser() {
+            return user;
+        }
+        private void setUser(String user) {
+            this.user = user;
+        }
+        private String getDevice() {
+            return device;
+        }
+        private void setDevice(String device) {
+            this.device = device;
+        }
+        private Socket getSocket() {
+            return socket;
+        }
+        private void setSocket(Socket socket) {
+            this.socket = socket;
+        }
+        private String getContent() {
+            return content;
+        }
+        private void setContent(String content) {
+            this.content = content;
+        }
+        private String getApp() {
+            return this.app;
+        }
+        private void setApp(String app) {
+            this.app = app;
+        }
+        private String getRaw() {
+            return raw;
+        }
+        private void setRaw(String raw) {
+            String[] notif = raw.split("#");
+            this.title = notif[2];
+            this.content = notif[3];
+            String[] userDev = notif[0].split("@");
+            this.user = userDev[0];
+            this.device = userDev[1];
+            this.app = notif[1];
+            this.raw = raw;
+        }
+
+        private String toBytes() {
+            return raw;
+        }
+        private void fromBytes(String input) {
+            String[] notif = input.split("#");
+            this.raw = input;
+            this.title = notif[2];
+            this.content = notif[3];
+            String[] userDev = notif[0].split("@");
+            this.user = userDev[0];
+            this.device = userDev[1];
+            this.app = notif[1];
+        }
+
+    }
+
+    public static class ConnectionSock {
         private Socket sock = null;
         private BufferedReader in = null;
         private PrintWriter out = null;
         private String name = null;
         private String notif = null;
         private String ip = null;
+        private String type = null;
 
         //getter for name
         private String getName() {
@@ -82,14 +159,20 @@ public class MyServer2 {
         private void setIP(String IP) {
             this.ip = IP;
         }
+        //getter for socket
+        private Socket getSock() {
+            return this.sock;
+        }
         //setter for socket, NOTE THIS IS NEEDED TO INITIALIZE
         private void setSock(Socket socket) {
             sock = socket;
             this.ip = sock.getRemoteSocketAddress().toString();
+        }   
+        private String getType() {
+            return this.type;
         }
-        //getter for socket
-        private Socket getSock() {
-            return this.sock;
+        private void setType(String type) {
+            this.type = type;
         }
 
         //Receives a packet
@@ -98,8 +181,15 @@ public class MyServer2 {
                 if(!sock.equals(null)) {
                     in = new BufferedReader(new InputStreamReader(sock.getInputStream()));
                     String received = in.readLine();
-                    String[] notification = received.split("\t");
+                    String[] notification = received.split("#");
                     if(notification[0].contains("@")) {
+                        if(notification[0].contains("PHONE:")) {
+                            type = "phone";
+                            notification[0] = notification[0].substring(6);
+                        }
+                        else {
+                            type = "desktop";
+                        }
                         name = notification[0];
                     }
                     else {
@@ -110,6 +200,7 @@ public class MyServer2 {
                 }
                 else {
                     System.out.println("Must initialize wiht a socket object!");
+                    return "0";
                 }
 
             } catch(IOException e) {
